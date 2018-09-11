18b22609b4b2c6470575b4071b83aa840f4e71a1
Vasil Sokolov <vasil.sokolov@gmail.com>
2018-09-11 11:01:26 +0300

    attempt to fix of script run from subfolder

--- travis.sh
+++ travis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+DIR=$(cd `dirname $0` && pwd)
 TASK="build"
 
 POSITIONAL=()
@@ -21,28 +22,28 @@ esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-echo TASK  = "$TASK"
+echo TASK: "$TASK"
 
 if [ "$TASK" = "build" ]; then
 	npm install -g gulp-cli
-	npm install ./modules/CoreWebclient
+	npm install ${DIR}/modules/CoreWebclient
 
 	gulp styles --themes Default,DeepForest,Funny --build a
 	gulp js:build --build a
 	gulp js:min --build a
 	gulp test
 	
-	PRODUCT_VERSION=`cat VERSION`
+	PRODUCT_VERSION=`cat ${DIR}/VERSION`
 	
-	echo CREATE ZIP FILE  = "${PRODUCT_NAME}_${PRODUCT_VERSION}.zip"
+	echo CREATE ZIP FILE: "${PRODUCT_NAME}_${PRODUCT_VERSION}.zip"
 	
 	zip -rq ${PRODUCT_NAME}_${PRODUCT_VERSION}.zip data/settings/modules modules static system vendor dev ".htaccess" dav.php index.php LICENSE VERSION README.md favicon.ico robots.txt composer.json composer.lock modules.json gulpfile.js pre-config.json -x **/*.bak *.git*
 fi
 
 if [ "$TASK" = "upload" ]; then
-	PRODUCT_VERSION=`cat VERSION`
+	PRODUCT_VERSION=`cat ${DIR}/VERSION`
 
-	echo UPLOAD ZIP FILE  = "${PRODUCT_NAME}_${PRODUCT_VERSION}.zip"
+	echo UPLOAD ZIP FILE: "${PRODUCT_NAME}_${PRODUCT_VERSION}.zip"
 	
 	curl --ftp-create-dirs -T ${PRODUCT_NAME}_${PRODUCT_VERSION}.zip -u ${FTP_USER}:${FTP_PASSWORD} ftp://afterlogic.com/
 fi
